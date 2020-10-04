0320fc8cb56480c2153fb28a783c235f5023a147
Tasos K <t.kozikopoulos@gmail.com>
2020-10-04 14:19:39 +0300

    Update urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='notes_index'),
     path('app/', views.app, name='notes_app'),
-    path('delete/<pk>', views.NoteDelete.as_view(), name="delete_note"),
+    path('delete/<pk>/', views.NoteDelete.as_view(), name="delete_note"),
 ]
